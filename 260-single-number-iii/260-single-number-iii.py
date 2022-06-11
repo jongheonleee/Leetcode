408888ee8553cf4c5a6b2136391699e74f1435ae
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-06-12 07:12:22 +0900

    Time: 111 ms (31.66%), Space: 15.5 MB (96.55%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.py
+++ 260-single-number-iii/260-single-number-iii.py
@@ -1,11 +1,7 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> List[int]:
-        cnt_map = collections.Counter(nums)
-        res = []
-        
-        for k, v in cnt_map.items():
-            if v == 1:
-                res.append(k)
-                
-        return res
+    def singleNumber(self, nums):
+        s = reduce(xor, nums)
+        nz = s & (s-1) ^ s
+        num1 = reduce(xor, filter(lambda n: n & nz, nums))
+        return(num1, s ^ num1)
         
