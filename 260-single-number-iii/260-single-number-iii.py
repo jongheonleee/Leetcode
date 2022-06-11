5a6d59c3287d5cd40fb00ede29e06a11670badd9
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-06-12 07:15:12 +0900

    Update 260-single-number-iii.py

--- 260-single-number-iii/260-single-number-iii.py
+++ 260-single-number-iii/260-single-number-iii.py
@@ -4,4 +4,15 @@ class Solution:
         nz = s & (s-1) ^ s
         num1 = reduce(xor, filter(lambda n: n & nz, nums))
         return(num1, s ^ num1)
+
+    def singleNumber_mine(self, nums: List[int]) -> List[int]:
+        # wrong ans: cuz i use O(n) in space complexity
+        cnt_map = collections.Counter(nums)
+        res = []
+        
+        for k, v in cnt_map.items():
+            if v == 1:
+                res.append(k)
+                
+        return res
         
