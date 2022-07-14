b43f469c644522a92d01a5580d04bf644d496d2e
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-07-15 01:24:49 +0900

    Time: 36 ms (88.12%), Space: 14.2 MB (14.60%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -5,7 +5,8 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def zigzagLevelOrder_(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # 1. approach > bfs
         if root is None:
             return []
         
@@ -30,5 +31,39 @@ class Solution:
             direction *= (-1)
             
         return res
+    
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # 2. approach > dfs(recursive)
+        ans = []
+        self.add_level(ans, 0, root)
+        return ans
+        
+    
+    
+    
+    
+    
+    def add_level(self, ans: list[list], level:int, root: Optional[TreeNode]):
+        if not root:
+            return 
+        
+        elif len(ans) <= level:
+            ans.append([root.val])
             
+        elif not level % 2:
+            # when level is even
+            # level ans should be original
+            # so that i used extend()
+            ans[level].extend([root.val])
+
+        else:
+            # when level is odd
+            # level ans should be inversed
+            # so that i used insert(0, val)
+            ans[level].insert(0, root.val)
+            
+        self.add_level(ans, level+1, root.left)
+        self.add_level(ans, level+1, root.right)
+        
+        
             
