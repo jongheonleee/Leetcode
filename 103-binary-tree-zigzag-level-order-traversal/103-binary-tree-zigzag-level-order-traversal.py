0bffd62bf9d6ddb6465ee22b9f0dd219a4509d5f
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-07-15 00:43:10 +0900

    Time: 36 ms (88.12%), Space: 14.1 MB (57.08%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -6,57 +6,29 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        '''        
-        res = []
-        
-        def traversal(node, cnt):
-            if node is None:
-                return
-            
-            res.append([node.val])
-            cnt += 1
-            
-            if cnt % 2:
-                traversal(node.right, cnt)
-                
-                if node.left:
-                    traversal(node.left, cnt)
-                
-            else:
-                traversal(node.left, cnt)
-                
-                if node.right:
-                    traversal(node.right, cnt)
-            return
-
-    
-        traversal(root, 0)
-        return res
-        '''
-        from collections import deque
-        
-        if not root:
+        if root is None:
             return []
         
-        q = deque([root])
-        result, direction = [], 1
+        res, direction = [], 1
+        
+        q = collections.deque([root])
         
         while q:
             level = []
             
-            for i in range(len(q)):
+            for _ in range(len(q)):
                 node = q.popleft()
                 level.append(node.val)
                 
                 if node.left:
                     q.append(node.left)
-                
+                    
                 if node.right:
                     q.append(node.right)
                     
-            result.append(level[::direction])
+            res.append(level[::direction])
             direction *= (-1)
             
-        return result
+        return res
             
             
