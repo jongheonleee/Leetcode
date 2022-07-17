579fbe344fdf8603c49aebb750f4d77f0d737903
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-07-17 17:17:16 +0900

    Time: 49 ms (44.81%), Space: 13.9 MB (75.43%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -11,16 +11,35 @@ class Solution:
         :type q: TreeNode
         :rtype: bool
         """    
-        # p and q are both None
-        if not p and not q:
+        from collections import deque
+        def check(p, q):
+            if not p and not q:
+                return True
+            
+            elif not p or not q:
+                return False
+            
+            elif p.val != q.val:
+                return False
+            
             return True
-        # one of p and q is None
-        if not q or not p:
-            return False
-        if p.val != q.val:
-            return False
-        return self.isSameTree(p.left, q.left) and \
-               self.isSameTree(p.right, q.right)
+            
+        
+        deq = deque([(p, q)])
+        
+        while deq:
+            p, q = deq.popleft()
+            
+            if not check(p, q):
+                return False
+            
+            if p and q:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+                
+                
+        return True
+                
                 
         
 
