dc1ef2dc6d0cd8ecf255e2a0f90d2c0d43af71be
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-07-22 06:39:02 +0900

    Time: 131 ms (5.04%), Space: 16.6 MB (5.43%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,16 +1,22 @@
 class Solution:
     def isSubsequence(self, s, t):
-        s, t = "!" + s, "!" + t
-        m, n = len(s), len(t)
-        dp = [[0] * m for _ in range(n)] 
-        for i in range(n): dp[i][0] = 1
-   
-        for i,j in product(range(1, n), range(1, m)):
+        s, t = ' ' + s, ' ' + t
+        
+        n, m = len(t), len(s)
+        dp = [[0] * m for _ in range(n)]
+        
+        for i in range(n):
+            dp[i][0] = 1
+            
+        for i, j in product(range(1, n), range(1, m)):
             if s[j] == t[i]:
                 dp[i][j] = dp[i-1][j-1]
+                
             else:
                 dp[i][j] = dp[i-1][j]
-                    
+                
         return dp[-1][-1]
+
+
         
         
