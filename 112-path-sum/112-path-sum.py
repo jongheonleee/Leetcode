f1bf4ae528a64b0439ace852764cbe4e743857cc
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-07-25 19:56:51 +0900

    Time: 41 ms (96.83%), Space: 15.1 MB (57.60%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -5,25 +5,80 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def hasPathSum1(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        '''
+        dfs recursively
+        '''
+        
+        res = []
+        def dfs(root, t):
+            if root:
+                if not root.left and not root.right and root.val == t:
+                    res.append(True)
+                    
+                if root.left:
+                    dfs(root.left, t-root.val)
+                    
+                if root.right:
+                    dfs(root.right, t-root.val)
+        
+        dfs(root, targetSum)
+        return any(res)
+    
+    def hasPathSum2(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        '''
+        dfs stack
+        '''
+        
+        if not root:
+            return False
+        
+        st = [(root, root.val)]
+        
+        while st:
+            crnt, val = st.pop()
+            
+            if not crnt.left and not crnt.right and val == targetSum:
+                return True
+            
+            if crnt.right:
+                st.append((crnt.right, val+crnt.right.val))
+                
+            if crnt.left:
+                st.append((crnt.left, val+crnt.left.val))
+                
+        return False
+    
+    def hasPathSum3(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        
+        q = [(root, targetSum-root.val)]
+        while q:
+            crnt, val = q.pop(0)
+            
+            if not crnt.left and not crnt.right and val == 0:
+                return True
+            
+            if crnt.left:
+                q.append((crnt.left, val-crnt.left.val))
+                
+            if crnt.right:
+                q.append((crnt.right, val-crnt.right.val))
+                
+        return False
+    
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        '''
-        return 을 못받음
+        if not root:
+            return False
         
-        '''
+        if not root.left and not root.right and root.val == targetSum:
+            return True
         
-        def dfs(node, t):
+        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
+    
+    
+                    
             
-            if not node:
-                return False
-            
-            t += node.val
-            
-            if not node.left and not node.right:
-                return t == targetSum
-            
-            return dfs(node.left, t) or dfs(node.right, t)
-        
-        return dfs(root, 0)
-
             
         
