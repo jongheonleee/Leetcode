b78996502fdeb8789fbf93354b3f5abdbbe1dc53
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-07-26 20:31:46 +0900

    Time: 988 ms (30.61%), Space: 15.3 MB (31.51%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -1,5 +1,5 @@
 class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
+    def islandPerimeter_(self, grid: List[List[int]]) -> int:
         m, n, P = len(grid), len(grid[0]), 0
         
         for i in range(m):
@@ -19,6 +19,40 @@ class Solution:
                     P -= grid[i][j]*grid[i][j+1]
                     
         return P
+    
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        r, c, q = len(grid), len(grid[0]), deque()
+        
+        for y in range(r):
+            for x in range(c):
+                if grid[y][x] == 1:
+                    q.append((y, x))
+                    return self.bfs(grid, r, c, q)
+                
+    def bfs(self, grid, r, c, q):
+        vis = set()
+        perimeter = 0
+        
+        while q:
+            y, x = q.popleft()
+            
+            if (y, x) in vis:
+                continue
+                
+            vis.add((y, x))
+            
+            for ny, nx in [(y+1, x), (y, x+1), (y-1, x), (y, x-1)]:
+                if 0<=ny<r and 0<=nx<c:
+                    if grid[ny][nx] == 0:
+                        perimeter += 1
+                        
+                    if grid[ny][nx] == 1:
+                        q.append((ny, nx))
+                        
+                else:
+                    perimeter += 1
+                    
+        return perimeter
 
         
        
