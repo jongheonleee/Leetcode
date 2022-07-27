97e88fb8918b83026f57bc692dd26f99e0d59650
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-07-27 12:21:31 +0900

    Time: 597 ms (60.01%), Space: 15 MB (67.96%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,4 +1,4 @@
-'''class Solution:
+class Solution_mine:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         
         res = float('-inf')
@@ -26,8 +26,8 @@
                 dfs(i, j, 1)
 
         return res
-        '''
-class Solution:
+        
+class Solution_DAG:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         rows = len(matrix)
         if rows == 0:
@@ -65,3 +65,27 @@ class Solution:
                                 queue.append((nx, ny))
             path_len += 1
         return path_len 
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        def dfs(i, j):
+            if not dp[i][j]:
+                val = matrix[i][j]
+                dp[i][j] = 1 + max(
+                    dfs(i-1, j) if i and val > matrix[i-1][j] else 0,
+                    dfs(i+1, j) if i < r-1 and val > matrix[i+1][j] else 0,
+                    dfs(i, j-1) if j and val > matrix[i][j-1] else 0,
+                    dfs(i, j+1) if j < c-1 and val > matrix[i][j+1] else 0)
+                
+            return dp[i][j]
+        
+        if not matrix or not matrix[0]:
+            return 0
+        
+        r, c = len(matrix), len(matrix[0])
+        dp = [[0] * c for _ in range(r)]
+        
+        
+        
+        return max(dfs(y, x) for y in range(r) for x in range(c))
+                
+                
