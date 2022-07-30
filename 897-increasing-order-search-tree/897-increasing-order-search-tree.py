ae5b78748d5356493db39df689ce6c95a73e236e
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-07-30 14:49:05 +0900

    Time: 62 ms (20.55%), Space: 14.2 MB (13.24%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.py
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.py
@@ -26,6 +26,29 @@ class Solution:
             
         return dummy_node.right
         
+    def increasingBST_1(self, root, tail = None):
+        if not root: return tail
+        res = self.increasingBST(root.left, root)
+        root.left = None
+        root.right = self.increasingBST(root.right, tail)
+        return res
+    
+    def increasingBST_2(self, root):
+        def dfs(node):
+            l1, r2 = node, node
+            
+            if node.left: 
+                l1, l2 = dfs(node.left)
+                l2.right = node
+                
+            if node.right:
+                r1, r2 = dfs(node.right)
+                node.right = r1
+            
+            node.left = None
+            return (l1, r2)
+        
+        return dfs(root)[0]
 
             
             
