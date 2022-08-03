bb522c62720e63a6e9cb348ff75fee5ebca17946
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-08-04 00:59:25 +0900

    Time: 65 ms (85.85%), Space: 15.7 MB (76.68%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.py
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.py
@@ -10,7 +10,7 @@ class Solution:
             return []
         
         return self.helper(1, n)
-
+    
     def helper(self, start, end):
         if start > end:
             return [None]
@@ -32,31 +32,9 @@ class Solution:
                     all_trees.append(crnt_root)
                     
         return all_trees
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-            
-# class Solution:
-#     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-#         @lru_cache(None)
-#         def dfs(left, right):
-#             if left > right: return [None]
-#             if left == right: return [TreeNode(left)]
-#             ans = []
-#             for root in range(left, right+1):
-#                 leftNodes = dfs(left, root - 1)
-#                 rightNodes = dfs(root+1, right)
-#                 for leftNode in leftNodes:
-#                     for rightNode in rightNodes:
-#                         rootNode = TreeNode(root, leftNode, rightNode)
-#                         ans.append(rootNode)
-#             return ans
         
-#         return dfs(1, n)
+        
+
                     
                     
                 
