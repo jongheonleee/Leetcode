01f954dc302349337f6823bb2b3d04de94518805
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-08-05 12:40:39 +0900

    Time: 153 ms (87.31%), Space: 16 MB (37.87%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -1,61 +1,37 @@
 class Solution:
-    def solve_(self, board: List[List[str]]) -> None:
+    def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-     
-        row, col = len(board), len(board[0])
-        cp_board = copy.deepcopy(board[:])
-        dir_ = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        
-        def bfs(y, x):
-            X_cnt = 0
-            for dy, dx in dir_:
-                ny, nx = y + dy, y + dx
-                
-                if ny < 0 or ny > len(board)-1 or nx < 0 or nx > len(board[0])-1:
-                    X_cnt += 1
-                
-                elif cp_board[ny][nx] == 'X':
-                    X_cnt += 1
-                    
+        def dfs(y, x):
+            if y < 0 or x < 0 or y >= M or x >= N or board[y][x] != 'O':
+                return
             
-            board[y][x] = 'O' if X_cnt == 4 else 'X'
-                        
+            board[y][x] = 'T'
+            
+            dir_ = [(y-1, x), (y+1, x), (y, x-1), (y, x+1)]
+            for ny, nx in dir_:
+                dfs(ny, nx)
+                
         
-        for r in range(row):
-            for c in range(col):
-                if board[r][c] == 'O':
-                    bfs(r, c)
-                    
-                    
-                    
-    def dfs(self, i, j):
-        if i < 0 or j < 0 or i >= self.M or j >= self.N or self.board[i][j] != 'O':
+        if board is None:
             return
         
-        self.board[i][j] = 'T'
-        next_yx = [[i+1, j], [i-1, j], [i, j-1], [i, j+1]]
+        M, N = len(board), len(board[0])
         
-        for ny, nx in next_yx:
-            self.dfs(ny, nx)
-            
-    def solve(self, board: List[List[str]]) -> None:
-        if not board:
-            return 0
         
-        self.board, self.M, self.N = board, len(board), len(board[0])
+        for y in range(M):
+            dfs(y, 0)
+            dfs(y, N-1)
+            
+        for x in range(N):
+            dfs(0, x)
+            dfs(M-1, x)
+            
+        for y, x in product(range(M), range(N)):
+            board[y][x] = 'X' if board[y][x] != 'T' else 'O'
+            
         
-        for i in range(0, self.M):
-            self.dfs(i, 0)
-            self.dfs(i, self.N-1)
-            
-        for j in range(0, self.N):
-            self.dfs(0, j)
-            self.dfs(self.M-1, j)
-            
-        for i, j in product(range(self.M), range(self.N)):
-            print(i, j)
-            board[i][j] = 'X' if board[i][j] != 'T' else 'O'
+        
                     
                     
