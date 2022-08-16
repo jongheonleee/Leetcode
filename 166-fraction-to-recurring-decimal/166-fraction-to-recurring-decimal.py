fd269e2aa2e5f9bf17d253a3724924b6bc4bb5e6
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-08-16 21:29:02 +0900

    Time: 60 ms (23.36%), Space: 13.7 MB (99.72%) - LeetHub

--- 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.py
+++ 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.py
@@ -4,6 +4,7 @@ class Solution:
         sign = '-' if numerator*denominator < 0 else ''
         result = [sign+str(n), '.']
         stack = []
+        
         while remainder not in stack:
             stack.append(remainder)
             n, remainder = divmod(remainder*10, abs(denominator))
@@ -14,30 +15,7 @@ class Solution:
         result.append(')')
         
         return ''.join(result).replace('(0)', '').rstrip('.')
-    
-    def fractionToDecimal(self, numerator, denominator):
-        if numerator % denominator == 0:
-            return str(numerator//denominator)
-        
-        sign = '' if numerator * denominator >= 0 else '-'
-        numerator, denominator = abs(numerator), abs(denominator)
-        res = sign + str(numerator//denominator) + '.'
-        numerator %= denominator
-        i, part = 0, ''
-        m = {numerator:i}
-        
-        while numerator % denominator:
-            numerator *= 10
-            i += 1
-            rem = numerator % denominator
-            part += str(numerator // denominator)
-            if rem in m:
-                part = part[:m[rem]]+'('+part[m[rem]:]+')'
-                return res + part
-            m[rem] = i
-            numerator = rem
-            
-        return res + part
+
 
 
                     
