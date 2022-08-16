21ce747dfd328bfaf4e7b48f7926d153660d42ff
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-08-16 21:34:27 +0900

    Time: 52 ms (42.31%), Space: 14 MB (32.24%) - LeetHub

--- 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.py
+++ 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.py
@@ -1,21 +1,22 @@
 class Solution:
     def fractionToDecimal(self, numerator, denominator):
-        n, remainder = divmod(abs(numerator), abs(denominator))
-        sign = '-' if numerator*denominator < 0 else ''
-        result = [sign+str(n), '.']
+        
+        n, reminder = divmod(abs(numerator), abs(denominator))
+        sign = '-' if numerator * denominator < 0 else ''
+        result = [sign + str(n), '.']
         stack = []
         
-        while remainder not in stack:
-            stack.append(remainder)
-            n, remainder = divmod(remainder*10, abs(denominator))
+        while reminder not in stack:
+            stack.append(reminder)
+            n, reminder = divmod(reminder * 10, abs(denominator))
             result.append(str(n))
 
-        idx = stack.index(remainder)
-        result.insert(idx+2, '(')
+            
+        idx = stack.index(reminder)
+        result.insert(idx + 2, '(')
         result.append(')')
         
         return ''.join(result).replace('(0)', '').rstrip('.')
-
 
 
                     
