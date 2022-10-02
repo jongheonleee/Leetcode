7f2c1bc0f926d552b515f321fa7b9610a570d74e
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-10-02 22:12:22 +0900

    Time: 645 ms (5.03%), Space: 19.2 MB (10.86%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,20 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # processing given string s
-        proc_s = ''
+        # extract alphabet/number in s then push list
+        lst = []
+        
         for char in s:
             if char.isalnum():
-                proc_s += char.lower()
+                lst.append(char.lower())
                 
-        # check if processing string s is palindrome or not
-        left, right = 0, len(proc_s)-1
-        while left <= right:
-            if proc_s[left] != proc_s[right]:
+        # check if lst is palindrome or not
+        while len(lst) > 1:
+            if lst.pop() != lst.pop(0):
                 return False
             
-            left += 1
-            right -= 1
-            
         return True
-            
         
