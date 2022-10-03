414698e8367413746a990afa449bfec52c01961e
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-10-03 21:36:52 +0900

    Time: 270 ms (94.53%), Space: 14 MB (57.51%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,20 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expand_window(left, right):
+        # dealing with exception case
+        if s == s[::-1] or len(s) < 2:
+            return s
+        
+        # expand window if window is palindrome
+        def expand_window(left:int, right:int) -> str:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-                
+            
+            # palindrom substring
             return s[left+1:right]
         
-        if len(s) < 2 and s == s[::-1]:
-            return s
+        # store longest palindrome
+        ans : str = ''
         
-        ans = ""
-        for i in range(0, len(s)):
-            ans = max(ans,
-                     expand_window(i, i+1),
-                     expand_window(i, i+2), 
-                     key = len)
         
+        for i in range(0, len(s)-1):
+            ans = max(
+                    ans,
+                    expand_window(i, i+1),
+                    expand_window(i, i+2),
+                    key=len)
+            
         return ans
