859746a693f8ac9db9ba1769c82291b9a242d8bf
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-10-03 21:43:35 +0900

    Time: 252 ms (94.79%), Space: 14 MB (28.00%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,11 +1,11 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # dealing with exception case
+        # exception case
         if s == s[::-1] or len(s) < 2:
             return s
         
-        # expand window if window is palindrome
-        def expand_window(left:int, right:int) -> str:
+        # expand two pointers if s[left:right] is palindrome
+        def expand_two_pointers(left:int, right:int) -> str:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
@@ -20,8 +20,8 @@ class Solution:
         for i in range(0, len(s)-1):
             ans = max(
                     ans,
-                    expand_window(i, i+1),
-                    expand_window(i, i+2),
+                    expand_two_pointers(i, i+1),
+                    expand_two_pointers(i, i+2),
                     key=len)
             
         return ans
