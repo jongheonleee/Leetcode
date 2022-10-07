9b41717c07a036d32edc3a04d0d9a83a37bbaae4
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-10-07 19:42:11 +0900

    Time: 1288 ms (67.06%), Space: 17.9 MB (85.42%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -3,36 +3,39 @@ class Solution:
         res = []
         nums.sort()
         
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-                
-            left, right = i+1, len(nums)-1
+        def find_target(sub_nums: List[int], target: int) -> None:
+            left, right = 0, len(sub_nums)-1
             
             while left < right:
-                sum = nums[i] + nums[left] + nums[right]
-                
-                if sum < 0:
-                    left += 1
+                if target == sub_nums[left] + sub_nums[right]:
+                    res.append([-target, sub_nums[left], sub_nums[right]])
                     
-                elif sum > 0:
-                    right -= 1
-                    
-                else:
-                    # sum == 0
-                    res.append([nums[i], nums[left], nums[right]])
-                    
-                    while left < right and nums[left] == nums[left+1]:
+                    while left < right and sub_nums[left] == sub_nums[left+1]:
                         left += 1
                         
-                    while left < right and nums[right] == nums[right-1]:
+                    while left < right and sub_nums[right] == sub_nums[right-1]:
                         right -= 1
                         
                     left += 1
                     right -= 1
-        
-        return res
                     
+                elif target > sub_nums[left] + sub_nums[right]:
+                    left += 1
+                    
+                else:
+                    right -= 1
+                    
+            
+            
+        for i, target in enumerate(nums):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+                
+            find_target(nums[i+1:], -target)
+            
+        return res
+                
+            
                     
                 
                 
