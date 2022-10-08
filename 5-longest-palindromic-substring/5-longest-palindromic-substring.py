786e4774edbe7ac18e185116f13eb3d4f6ed3e57
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-10-08 22:51:53 +0900

    Time: 2027 ms (33.10%), Space: 13.9 MB (57.39%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,27 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         # exception case
-        if s == s[::-1] or len(s) < 2:
+        if len(s) < 2 and s == s[::-1]:
             return s
         
-        # expand two pointers if s[left:right] is palindrome
-        def expand_two_pointers(left:int, right:int) -> str:
+        # for saving longest palindrome substring
+        ans : str = ''
+        
+        def expand(left: int, right: int) -> str:
+            # extract palindromic substring
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            
-            # palindrom substring
+                
             return s[left+1:right]
-        
-        # store longest palindrome
-        ans : str = ''
-        
+                
         
         for i in range(0, len(s)-1):
-            ans = max(
-                    ans,
-                    expand_two_pointers(i, i+1),
-                    expand_two_pointers(i, i+2),
-                    key=len)
+            ans = max(ans,
+                     expand(i, i+1),
+                     expand(i, i+2),
+                     key=len)
             
         return ans
+            
+        
