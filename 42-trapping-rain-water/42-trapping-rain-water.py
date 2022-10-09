b3459cc157455b2e0c8ba84f1f354d1a947b9fee
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-10-09 21:18:39 +0900

    Time: 317 ms (26.16%), Space: 15.9 MB (95.64%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,17 +1,30 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l_max, r_max, l, r = height[0], height[-1], 0, len(height)-1
-        water = 0
+        # those variable pointed a highest wall which i passed
+        # max_l => left side
+        # max_r => right side        
+        max_l: int = 0
+        max_r: int = 0
+            
+        left, right = 0, len(height)-1
         
-        while l < r:
-            if height[l] <= height[r]:
-                l += 1
-                l_max = max(l_max, height[l])
-                water += (l_max - height[l])
+        # store how much water can i trap
+        water: int = 0
+        
+        # using two pointers
+        while left < right:
+            if height[left] <= height[right]:
+                max_l = max(max_l, height[left])
+                water += max_l - height[left]
+                
+                left += 1
                 
             else:
-                r -= 1
-                r_max = max(r_max, height[r])
-                water += (r_max - height[r])
+                max_r = max(max_r, height[right])
+                water += max_r - height[right]
+                
+                right -= 1
                 
         return water
+                    
+
