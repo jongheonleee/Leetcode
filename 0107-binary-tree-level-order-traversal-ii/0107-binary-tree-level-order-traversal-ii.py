5cd601c3449ab74bb915736bb481a967538fb907
yeonuel <87258372+jongheonleee@users.noreply.github.com>
2022-10-26 12:53:24 +0900

    Time: 85 ms (5.75%), Space: 14.2 MB (49.67%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.py
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.py
@@ -49,7 +49,7 @@ class Solution:
             
         return None
     
-    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def levelOrderBottom3(self, root: Optional[TreeNode]) -> List[List[int]]:
         # using dfs by stack
         # tuple => (node, level)
         stack, res = [(root, 0)], []
@@ -64,5 +64,23 @@ class Solution:
                 stack.append((node.left, level+1))
             
         return res
+    
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        queue:Deque = collections.deque([(root, 0)])
+        res = []
+        
+        while queue:
+            node, level = queue.popleft()
+            if node:
+                if len(res) < level+1:
+                    res.insert(0, [])
+                    
+                res[-(level+1)].append(node.val)
+                queue.append([node.left, level+1])
+                queue.append([node.right, level+1])
+
+        return res            
+        
+        
         
         
